c9137ac358a371cffbc1d57e61de7bd23ce1f8d0
Abu Hasan Rumi <message.rumi@gmail.com>
2020-09-23 10:38:26 +0600

    book at burj al arab

---
+++ src/components/Login/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBn0Tfu0W2SK1uF89qQjpDNpS7oUlZDPIo",
+    authDomain: "burj-al-arab-2.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-2.firebaseio.com",
+    projectId: "burj-al-arab-2",
+    storageBucket: "burj-al-arab-2.appspot.com",
+    messagingSenderId: "60985232806",
+    appId: "1:60985232806:web:d5c894436c06bfed885367"
+  };
+
+  export default firebaseConfig;
